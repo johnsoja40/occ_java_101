5fd303c0eb28a22aa17c6a0b416e19584471e9bf
cwcis <cwcis@occnt.ad>
2020-02-25 17:55:27 -0500

    committing from last week.

---
+++ ClassWork.java
@@ -0,0 +1,64 @@
+/*
+   name:       Jamie Johnson
+   Assignment: 
+   Problem:
+*/
+
+import java.util.Scanner;
+
+public class ClassWork
+{
+   public static void main(String args[])
+   {
+   
+      int count = 2;
+      
+      while ( count-- > 0 )
+      {
+         System.out.println("count is " + count);
+         //count++;
+      }
+      
+      System.out.println("\nThat's all, bitches!");
+   }
+}
+
+
+
+
+
+
+d = 10
+a = 5
+c = 4
+x = 6
+
+while ( x-- > 1)
+   switch(x)
+      case 3:
+      case 4:  a *= c; 
+      case 5:  d += 2  
+               break
+      case 6:  c += d--
+               break
+      default  a--
+               d++
+               c++
+
+
+# extra credit
+# 1
+given any odd value great then 1; input value is 3
+
+ *
+***
+ *
+only print out '*' or 'b' where b in blank
+ 
+# 2
+Given an 2 integers both greater than 2; input is 4 and 4
+**** 
+*  *
+*  *
+*  *
+****
